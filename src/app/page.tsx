69698eeab04c0b9890387d48f96332accd890b94
Chukwunonso Ikeji <williamikeji@gmail.com>
2023-10-11 17:49:37 +0100

    gabbabge file cleared (#5)
    * gabbabge file cleared
    
    * build error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,14 @@
-import Image from "next/image";
-import Hero from "./components/home/Hero";
-import About from "./components/home/About";
-import Demo from "./components/home/Demo";
-import Features from "./components/home/Features";
-import Token from "./components/home/Token";
-import Team from "./components/home/Team";
-import Action from "./components/home/Action";
-import Footer from "./components/home/Footer";
+"use client"
+import { redirect } from 'next/navigation'
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    redirect('/signup')
+  })
   return (
     <main className="bg-primary_13">
-      <Hero />
-      <About />
-      <Demo />
-      <Features />
-      <Token />
-      {/* <Team /> */}
-      <Action />
-      <Footer />
+
     </main>
   );
 }
