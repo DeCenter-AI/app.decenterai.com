d79a57fe5e30376cf1cb8f222ddcbda952a8b835
Nasfame <laciferin@gmail.com>
2023-10-14 03:14:32 +0530

    wip: compute test
    Took 14 minutes

---
+++ utils/cid.ts
@@ -0,0 +1,25 @@
+import { CID } from "multiformats/cid";
+
+
+
+export function isCid(cidStr:string){
+    const cid = CID.parse(cidStr);
+
+    if (cid) {
+        console.log("The string is a CID.");
+    } else {
+        console.log("The string is not a CID.");
+    }
+
+    return !!cid
+}
+
+
+function main(){
+    const cid = "bafybeigdyr2376427363276243276242326273262";
+    isCid(cid)
+
+    isCid("hiro")
+}
+
+main()
