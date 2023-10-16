3a7b019dbc9bf9486de35f0174c434d591865581
techyNonso <williamikeji@gmail.com>
2023-10-16 13:05:05 +0100

    hard coded cid removed

--- utils/fileCoinIpfs.ts
+++ utils/fileCoinIpfs.ts
@@ -1,41 +1,41 @@
-import CID from "cids";
-import { saveAs } from "file-saver";
-import JSZip from "jszip";
-import { Web3Storage } from "web3.storage";
+import CID from 'cids'
+import { saveAs } from 'file-saver'
+import JSZip from 'jszip'
+import { Web3Storage } from 'web3.storage'
 
 //decode CID to right format
 export const decodeCid = (data: string) => {
-  const cid = new CID(data.toString());
+  const cid = new CID(data.toString())
 
-  const decodedCid = cid.toV1().toBaseEncodedString("base32");
+  const decodedCid = cid.toV1().toBaseEncodedString('base32')
 
-  return decodedCid;
-};
+  return decodedCid
+}
 
 export const retrieve = async (cid: string, modelName: string) => {
   const client = new Web3Storage({
     token: `${process.env.NEXT_PUBLIC_WEB_TOKEN}`,
-  });
-  const res = await client.get(cid);
-  let files = await res.files();
-  console.log(files);
+  })
+  const res = await client.get(cid)
+  let files = await res.files()
+  console.log(files)
 
   if (files.length > 0) {
     //fetch the output folder
-    const outputRes = await client.get(files[1].cid);
-    let outputFiles = await outputRes.files();
-    console.log(outputFiles);
+    const outputRes = await client.get(files[1].cid)
+    let outputFiles = await outputRes.files()
+    console.log(outputFiles)
     //zip the file
-    let zip = new JSZip();
-    zip.file("output.zip", outputFiles[0]);
-    zip.generateAsync({ type: "blob" }).then((content) => {
-      console.log(content);
+    let zip = new JSZip()
+    zip.file('output.zip', outputFiles[0])
+    zip.generateAsync({ type: 'blob' }).then((content) => {
+      console.log(content)
       //save to device
-      saveAs(content, `${modelName}`);
-    });
+      saveAs(content, `${modelName}`)
+    })
 
-    return true;
+    return true
   } else {
-    return false;
+    return false
   }
-};
+}
