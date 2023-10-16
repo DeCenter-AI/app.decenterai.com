9c8b25d55b98f57135e27486c81a332aca219569
Chukwunonso Ikeji <williamikeji@gmail.com>
2023-10-16 23:42:54 +0100

    feat/be: integrate train model api [decenter-compute] (#19)
    * error with file upload
    
    * lighthouse upload fixed, blocked by cors error with compute
    
    * lighthouse upload fixed, blocked by cors error with compute
    
    * train flow optimized with modal prompts
    
    * compute run now successfull
    
    * ui changed
    
    * ipynb file enforced
    
    * download implimented but ipfs fails
    
    * successful downloading implemented using one working cid
    
    * :pushpin: add sharp for image optimization
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 15 seconds
    
    * :bug: use client missing
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 5 minutes
    
    * add: compute samples
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes
    
    * :pushpin: reactdevtools
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 5 minutes
    
    * minor:
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 14 minutes
    
    * chore: wip: vercel
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 18 minutes
    
    * chore: wip: vercel
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 1 minute
    
    * fix: tsconfig.json
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes
    
    * fix: vercel deployment
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 4 minutes
    
    * remove: legacy routes
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 10 minutes
    
    * package.json: pnpm@8.6.6
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 5 minutes
    
    * chore: vercel.json: remove functions
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 50 seconds
    
    * auto select of train script done
    
    * react-select removed
    
    * hard coded a working CID
    
    * logo adjusted
    
    * style: pretty code
    
    automated via ci
    
    * remove: routes
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes
    
    * fix: vercel
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    * scripts: clean
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    * style: pretty code
    
    automated via ci
    
    * ignore: .vercel
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    * revert: to .next build directory
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    * style: pretty code
    
    automated via ci
    
    * hard coded cid removed
    
    * style: pretty code
    
    automated via ci
    
    ---------
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    Co-authored-by: Nasfame <laciferin@gmail.com>
    Co-authored-by: Nasfame <Nasfame@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,21 @@
-import "./globals.css";
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import '@rainbow-me/rainbowkit/styles.css'
-import {Providers} from './providers'
-import {ContextProvider} from "./userContext";
-import Favicon from '@public/meta/favicon.ico';
+import { Providers } from './providers'
+import { ContextProvider } from './userContext'
+import Favicon from '@public/meta/favicon.ico'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "DecenterAI",
-  description: "DecenterAI: Decentralized Infra for AI Model Training",
+  title: 'DecenterAI',
+  description: 'DecenterAI: Decentralized Infra for AI Model Training',
   icons: [{ rel: 'icon', url: Favicon.src }],
-    manifest: '/meta/site.webmanifest',
-};
+  manifest: '/meta/site.webmanifest',
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -28,5 +24,5 @@ export default function RootLayout({
         </ContextProvider>
       </body>
     </html>
-  );
+  )
 }
--- src/app/userContext.js
+++ src/app/userContext.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { createContext, useState, useContext } from 'react'
+import React, {createContext, useContext, useState} from 'react'
 
 const AppContext = createContext()
 
