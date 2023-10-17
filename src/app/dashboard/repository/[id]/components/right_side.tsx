a3403a62d6c63e28a0778d8c0e1b5568e1e27f7f
Raji Roqeeb Olalekan <rajcodes733@gmail.com>
2023-10-17 21:27:38 +0100

    updated

--- src/app/dashboard/repository/[id]/components/right_side.tsx
+++ src/app/dashboard/repository/[id]/components/right_side.tsx
@@ -4,30 +4,6 @@ import Image from 'next/image'
 import Modal from './modal'
 function RightSide() {
   return (
-<<<<<<< HEAD
-    <div className=' h-[100%] max-h-screen  overflow-y-scroll px-4 '>
-        <div className='mb-4'>
-            <Image className=' justify-center grid' src={img} alt='' />
-        </div>
-        <div className='px-4 py-2 mb-4 text-center bg-primary_11 rounded-full'>
-            <span className='text-xs'>Edit Model</span>
-        </div>
-        <div className='px-4 py-2 mb-4  bg-primary_11 rounded-2xl'>
-            <ul>
-            <li className='text-xs px-3 py-3 hover:bg-black hover:rounded-full'>Model</li>
-            <li className='text-xs text-[#5D5D5D]  px-3  py-3 hover:bg-black hover:rounded-full'>Description</li>
-            <li className='text-xs text-[#5D5D5D]  px-3  py-3 hover:bg-black hover:rounded-full'>Version history</li>
-            <li className='text-xs text-[#5D5D5D]  px-3  py-3 hover:bg-black hover:rounded-full'>Datasety</li>
-            </ul>
-        </div>
-        <div className='px-4 py-2 mb-4  bg-primary_11 rounded-2xl'>
-            <ul>
-            <li className='text-xs px-3 text-[#5D5D5D] py-3 hover:bg-black hover:rounded-full'>Created Sep, 12 2023</li>
-            <li className='text-xs  px-3 text-[#5D5D5D]  py-3 hover:bg-black hover:rounded-full'>Downloads</li>
-           
-            </ul>
-        </div>
-=======
     <div className=" h-[100%] max-h-screen  overflow-y-scroll px-4 ">
       <div className="mb-4">
         <Image className=" justify-center grid" src={img} alt="" />
@@ -59,13 +35,8 @@ function RightSide() {
           </li>
         </ul>
       </div>
->>>>>>> 9c8b25d55b98f57135e27486c81a332aca219569
     </div>
   )
 }
 
-<<<<<<< HEAD
 export default RightSide
-=======
-export default RightSide
->>>>>>> 9c8b25d55b98f57135e27486c81a332aca219569
