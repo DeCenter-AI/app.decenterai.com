8dd444d4e71ab99d224a7bf6220b8e48de360fef
Nasfame <laciferin@gmail.com>
2023-10-19 11:01:58 +0530

    migrations:
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 8 minutes

--- prisma/migrations/20231019051912_delete_data_store_id/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `dataStoreId` on the `training_requests` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "training_requests" DROP COLUMN "dataStoreId";
-ALTER TABLE "training_requests" ALTER COLUMN "modelId" DROP NOT NULL;
