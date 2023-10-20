2e40dd0d691c6bc3caccd70965893f339be74fb2
Nasfame <laciferin@gmail.com>
2023-10-20 13:58:12 +0530

    jestconfig: ignore mjs
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
-import { pathsToModuleNameMapper } from 'ts-jest'
-import { compilerOptions } from './tsconfig.node.json'
+import {pathsToModuleNameMapper} from 'ts-jest'
+import {compilerOptions} from './tsconfig.node.json'
 
 const jestConfig = {
   // preset: 'ts-jest',
@@ -9,8 +9,8 @@ const jestConfig = {
   testEnvironment: 'node',
   collectCoverageFrom: ['**/*.(t|j|mj)s'],
   coverageDirectory: '../coverage',
-  testRegex: '.*\\.spec\\.(t|j|mj)s$',
-  moduleFileExtensions: ['js', 'json', 'ts', 'mjs'],
+  testRegex: '.*\\.spec\\.(t|j)s$',
+  moduleFileExtensions: ['js', 'json', 'ts'],
   // automock: true,
   verbose: true,
 }
