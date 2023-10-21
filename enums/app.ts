2ecc239cd93d109b49641b27d2a110bf7a0587ac
Nasfame <laciferin@gmail.com>
2023-10-21 06:07:11 +0530

    refactor: code
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 9 minutes
    
    
    Took 16 seconds

---
+++ enums/app.ts
@@ -0,0 +1,6 @@
+export const AppName = "Decenter AI"
+export const AppDescription = `
+DecenterAI: Decentralized Infra for AI Model Training
+`.trim()
+
+export const AppSiteManifest = '/site.webmanifest'
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
 import '@rainbow-me/rainbowkit/styles.css'
-import { Providers } from './providers'
-import { ContextProvider } from './userContext'
-import Favicon from '@public/meta/favicon.ico'
+// @ts-ignore
+import {Providers} from './providers'
+import {ContextProvider} from './userContext'
+import Favicon from '@public/favicon.ico'
+import {AppDescription, AppName, AppSiteManifest} from "../../enums/app";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'DecenterAI',
-  description: 'DecenterAI: Decentralized Infra for AI Model Training',
+  title: AppName,
+  description: AppDescription,
   icons: [{ rel: 'icon', url: Favicon.src }],
-  manifest: '/meta/site.webmanifest',
+  manifest: AppSiteManifest,
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
