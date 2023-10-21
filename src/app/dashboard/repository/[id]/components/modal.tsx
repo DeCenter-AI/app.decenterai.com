ef65ebe52123a436a2723a797b27588ad7fb5833
Nasfame <laciferin@gmail.com>
2023-10-22 05:15:20 +0530

    fix: merge conflict marker
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 22 minutes

--- src/app/dashboard/repository/[id]/components/modal.tsx
+++ src/app/dashboard/repository/[id]/components/modal.tsx
@@ -1,65 +1,6 @@
-<<<<<<< HEAD
-"use client"
-import {useSearchParams} from 'next/navigation'
-import {useRef , useEffect} from 'react'
-
-type Props = {
-    title : string,
-    onClose: () => void,
-    onOk: () => void,
-    children : React.ReactNode,
-
-}
-
-import React from 'react'
-
-export default function Modal({title, onClose, onOk, children}: Props) {
-
-    const searchParams = useSearchParams()
-    const dialogRef = useRef<null | HTMLDialogElement>(null)
-    const showDialog = searchParams.get('showDialog')
-
-    useEffect(()=>{
-        if(showDialog === 'y'){
-            dialogRef.current?.showModal()
-        }else{
-            dialogRef.current?.close()
-        }
-    }, [showDialog])
-
-    const closeDialog = () =>{
-        dialogRef.current?.close()
-        onClose() 
-    }
-
-    const clickOk =()=>{
-        onOk()
-        closeDialog()
-    }
-
-    const dialog: JSX.Element | null = showDialog === 'y'
-    ?(
-        <dialog ref={dialogRef} className='fixed top-50 left-50 -translate-x-52 translate-y-52 z-10 backdrop:bg-gray-800/50'>
-             <div>
-                 <div>
-                    <h1>{title}</h1> 
-                    <button onClick={closeDialog}>X</button>
-                 </div>
-                 <div>
-                    {children}
-                    <div>
-                        <button onClick={clickOk}>Ok</button>
-                    </div>
-                 </div>
-             </div>
-        </dialog>
-    ) : null
-
-     
-=======
 'use client'
-import { useSearchParams } from 'next/navigation'
-import React, { useEffect, useRef } from 'react'
+import {useSearchParams} from 'next/navigation'
+import React, {useEffect, useRef} from 'react'
 
 type Props = {
   title: string
@@ -112,6 +53,5 @@ export default function Modal({ title, onClose, onOk, children }: Props) {
       </dialog>
     ) : null
 
->>>>>>> 403f67bcc1b7899b225e01aaad7eb6864adde427
   return dialog
 }
