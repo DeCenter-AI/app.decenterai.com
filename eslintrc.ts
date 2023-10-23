22659859f54e10113e8c2c6690767c4bb9a38b1c
utkarsh-1602 <utkarsh-1602@users.noreply.github.com>
2023-10-23 07:03:42 +0000

    style: pretty code
    automated via ci

--- eslintrc.ts
+++ eslintrc.ts
@@ -1,14 +1,12 @@
 module.exports = {
-    extends: ['next'],
-    parserOptions: {
-      project: './tsconfig.json', 
-    },
-    rules: {
-        "indent": ["error", 2],
-        "semi": ["error", "always"],
-        "quotes": ["error", "single"],
-        "comma-dangle": ["error", "always-multiline"]
-      }
-      
-  };
-  
+  extends: ['next'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    indent: ['error', 2],
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+}
