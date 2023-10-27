5fa85a88f5f052a543f3ce37cf0fc1dee4b8a829
utkarsh-1602 <usking619@gmail.com>
2023-10-28 01:22:55 +0530

    hardhat setup

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,19 +1,8 @@
-import { HardhatUserConfig } from 'hardhat/config'
-import '@nomicfoundation/hardhat-toolbox'
-
-import dotenv from 'dotenv'
-
-dotenv.config()
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.19',
-  networks: {
-    // Add your network configurations here
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.SEPOLIA_PRIVATE_KEY ?? ''],
-    },
-  },
-}
+  solidity: "0.8.19",
+};
 
-export default config
+export default config;
