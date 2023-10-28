2a08119dd13ac4902fa91fd2a7544069949138d0
techyNonso <techyNonso@users.noreply.github.com>
2023-10-28 14:27:40 +0000

    style: pretty code
    by ci

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,46 +1,48 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { DashLayout } from '../dashLayout';
+import { DashLayout } from '../dashLayout'
 import { BsPerson } from 'react-icons/bs'
-import Link from 'next/link';
+import Link from 'next/link'
 import { GoBell } from 'react-icons/go'
-import NotificationModal from './components/notificationModal';
+import NotificationModal from './components/notificationModal'
 
 import { BiWallet, BiLock, BiLogOut } from 'react-icons/bi'
-import SecurityModal from './components/securityModal';
+import SecurityModal from './components/securityModal'
 export default function Modal() {
-  const [showSecurityModal, setShowSecurityModal] = useState(false);
-  const [showNotificationModal, setShowNotificationModal] = useState(false);
+  const [showSecurityModal, setShowSecurityModal] = useState(false)
+  const [showNotificationModal, setShowNotificationModal] = useState(false)
 
-const openNotificationModal = () => {
-  setShowNotificationModal(true);
-};
+  const openNotificationModal = () => {
+    setShowNotificationModal(true)
+  }
 
-const closeNotificationModal = () => {
-  setShowNotificationModal(false);
-};
-const openSecurityModal = () => {
-  setShowSecurityModal(true);
-};
+  const closeNotificationModal = () => {
+    setShowNotificationModal(false)
+  }
+  const openSecurityModal = () => {
+    setShowSecurityModal(true)
+  }
 
-const closeSecurityModal = () => {
-  setShowSecurityModal(false);
-};
+  const closeSecurityModal = () => {
+    setShowSecurityModal(false)
+  }
 
   return (
     <>
       <DashLayout>
         <div className="h-full w-full px-4 flex gap-5 pt-10 text-white">
           <div className="w-[80%]">
-            <button className="rounded-2xl border mb-4 w-full gap-2 items-cente border-primary_9 flex px-4 py-5" >
+            <button className="rounded-2xl border mb-4 w-full gap-2 items-cente border-primary_9 flex px-4 py-5">
               <div className="text-primary_8">
                 <BsPerson size={24} />
               </div>
               <span>Profile </span>
             </button>
-            <button className="rounded-2xl w-full border mb-4 gap-2 items-cente border-primary_9 flex px-4 py-5" type="button"
-             onClick={openSecurityModal}
-             >
+            <button
+              className="rounded-2xl w-full border mb-4 gap-2 items-cente border-primary_9 flex px-4 py-5"
+              type="button"
+              onClick={openSecurityModal}
+            >
               <div className="text-primary_8">
                 <BiLock size={24} />
               </div>
@@ -52,15 +54,16 @@ const closeSecurityModal = () => {
               </div>
               <span>Wallet connect</span>
             </button>
-            <button className="rounded-2xl w-full border mb-4 gap-2 items-center border-primary_9 flex px-4 py-5"  onClick={openNotificationModal}>
+            <button
+              className="rounded-2xl w-full border mb-4 gap-2 items-center border-primary_9 flex px-4 py-5"
+              onClick={openNotificationModal}
+            >
               <div className="text-primary_8">
                 <GoBell size={24} />
               </div>
 
               <span>Notification</span>
             </button>
-          
-
           </div>
           <div className="w-[20%]">
             <button className="flex bg-primary_9 items-center gap-3 text-white  w-full justify-center py-2 rounded-xl">
@@ -70,19 +73,13 @@ const closeSecurityModal = () => {
               <span className="text-xs">Log Out</span>
             </button>
           </div>
-
         </div>
-        <NotificationModal show={showNotificationModal} onClose={closeNotificationModal} />
-        <SecurityModal show={showSecurityModal} onClose={closeSecurityModal}  />
-       
-
-
-      
+        <NotificationModal
+          show={showNotificationModal}
+          onClose={closeNotificationModal}
+        />
+        <SecurityModal show={showSecurityModal} onClose={closeSecurityModal} />
       </DashLayout>
-
-
     </>
-  );
+  )
 }
-
-
