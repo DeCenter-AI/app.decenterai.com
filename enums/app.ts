47f505f69f09befd08f77536062ce2abe7df1016
Nasfame <laciferin@gmail.com>
2023-10-29 07:24:28 +0530

    refactor: metadata
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 9 minutes

--- enums/app.ts
+++ enums/app.ts
@@ -1,12 +1,36 @@
-import { CHAIN_NAMESPACES } from '@web3auth/base'
-import { Web3AuthNoModalOptions } from '@web3auth/no-modal'
+import {CHAIN_NAMESPACES} from '@web3auth/base'
+import {Web3AuthNoModalOptions} from '@web3auth/no-modal'
+import {Metadata} from "next";
+import Favicon from "@public/favicon.ico";
+import Favicon16x16 from "@public/favicon-16x16.png";
+import Favicon32x32 from "@public/favicon-32x32.png";
+import SafariPinnedTab from "@public/meta/safari-pinned-tab.svg";
+import AppleTouchIcon from "@public/meta/apple-touch-icon.png";
 
 export const AppName = 'Decenter AI'
-export const AppDescription = `
+const AppDescription = `
 DecenterAI: Decentralized Infra for AI Model Training
 `.trim()
 
-export const AppSiteManifest = '/meta/site.webmanifest'
+export const AppMetaData: Metadata = {
+  applicationName: AppName,
+  authors: [{name: 'Hiro', url: 'https://bit.ly/m/laciferin'}],
+  keywords: ['DecenterAI', 'AI Infra'],
+  title: AppName,
+  description: AppDescription,
+  icons: [
+    {rel: 'icon', url: Favicon.src},
+    {rel: 'icon', sizes: '16x16', url: Favicon16x16.src},
+    {rel: 'icon', sizes: '32x32', url: Favicon32x32.src},
+    {rel: 'mask-icon', url: SafariPinnedTab.src, color: '#5bbad5'},
+    {rel: 'apple-touch-icon', url: AppleTouchIcon.src},
+  ],
+  manifest: '/meta/site.webmanifest',
+  themeColor: '#ffffff',
+  other: {
+    'msapplication-TileColor': '#da532c',
+  },
+}
 
 export const WagmiProjectId = 'db1b8a46ffa835bd9a48a89ff540f990'
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,13 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import '@rainbow-me/rainbowkit/styles.css'
-import { Providers } from './providers'
-import { ContextProvider } from './userContext'
-import Favicon from '@public/favicon.ico'
-import Favicon16x16 from '@public/favicon-16x16.png'
-import Favicon32x32 from '@public/favicon-32x32.png'
-import SafariPinnedTab from '@public/meta/safari-pinned-tab.svg'
-import AppleTouchIcon from '@public/meta/apple-touch-icon.png'
-import { AppDescription, AppName, AppSiteManifest } from '@enums/app'
+import {Providers} from './providers'
+import {ContextProvider} from './userContext'
+import {AppMetaData} from "@enums/app";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  applicationName: AppName,
-  authors: [{ name: 'Hiro', url: 'https://bit.ly/m/laciferin' }],
-  keywords: ['DecenterAI', 'AI Infra'],
-  title: AppName,
-  description: AppDescription,
-  icons: [
-    { rel: 'icon', url: Favicon.src },
-    { rel: 'icon', sizes: '16x16', url: Favicon16x16.src },
-    { rel: 'icon', sizes: '32x32', url: Favicon32x32.src },
-    { rel: 'mask-icon', url: SafariPinnedTab.src, color: '#5bbad5' },
-    { rel: 'apple-touch-icon', url: AppleTouchIcon.src },
-  ],
-  manifest: AppSiteManifest,
-  themeColor: '#ffffff',
-  other: {
-    'msapplication-TileColor': '#da532c',
-  },
-}
+export const metadata = AppMetaData
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
