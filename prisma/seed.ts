fb840d1d3c832034f01c2439b622e08e58fdd176
Nasfame <laciferin@gmail.com>
2023-10-31 04:00:36 +0530

    Revert "fix:"
    This reverts commit 59a3cde3210a46c99d0c451503f113bfcf862750.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import {PrismaClient} from '@prisma/client'
-import {IPFSProviders, LIGHTHOUSE} from '../enums/ipfsProviders'
+import {IPFSProviders, LIGHTHOUSE} from '@enums/ipfsProviders'
 
 const prisma = new PrismaClient()
 
