c2fc8e6617b79299cc80dd4f2b2389ffe0ff9b8f
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-02 02:56:09 +0100

    fix: change profile button to link

---
+++ src/app/dashboard/settings/profile/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DashLayout } from '../../dashLayout'
+
+const page = () => {
+  return (
+      <DashLayout>
+          <div>
+              <h1>Hello world</h1>
+          </div>
+    </DashLayout>
+  )
+}
+
+export default page
