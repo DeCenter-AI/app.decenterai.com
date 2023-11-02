0424ed0613a4591bba53c5e1cd437b22cea2faa3
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-02 19:45:49 +0100

    feat: add types for profile data

--- src/app/dashboard/settings/profile/page.tsx
+++ src/app/dashboard/settings/profile/page.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { DashLayout } from '../../dashLayout'
+'use client'
 
-const page = () => {
+import { useRouter } from 'next/navigation'
+import { DashLayout } from '../../dashLayout'
+import {PiCaretLeft} from "react-icons/pi"
+export default function Page() {
+  const router = useRouter()
+
   return (
-      <DashLayout>
-          <div>
-              <h1>Hello world</h1>
-          </div>
+    <DashLayout>
+      <section>
+        <div className='flex gap-4  items-center'>
+          <button
+            type="button"
+            onClick={() => router.push('/dashboard/settings/profile')} className='flex gap-2 items-center'>
+            <PiCaretLeft/>
+            <span>Back</span>
+          </button>
+        </div>
+      </section>
     </DashLayout>
   )
 }
-
-export default page
---
+++ src/data/profile.ts
@@ -0,0 +1,14 @@
+interface Info {
+  id: number
+  text: string
+  count: number
+  icon: React.ReactNode
+}
+interface Model {
+  title: string
+  name: string
+  desc: string
+  date: string
+  download: number
+  like: number
+}
