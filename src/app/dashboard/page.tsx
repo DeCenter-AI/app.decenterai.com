f03c4ae365a3203bb33ecfc1d03ecc383f68265f
Codebox124 <Codebox124@users.noreply.github.com>
2023-11-02 20:16:08 +0000

    style: pretty code
    by ci

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -84,13 +84,17 @@ export default function Page() {
             <span className="text-lg">Recently created models</span>
           </div>
           <div className="w-full overflow-y-scroll   max-h-full px-8 py-8">
-            <div className='flex flex-col justify-center items-center'>
-
-             <div className='mb-2'>
-             <Image src={Emptyimg} className='grayscale' height={136} width={171.67} alt='' />
-             </div>
-              <span className='text-center text-xs '>No models created yet.</span>
-
+            <div className="flex flex-col justify-center items-center">
+              <div className="mb-2">
+                <Image
+                  src={Emptyimg}
+                  className="grayscale"
+                  height={136}
+                  width={171.67}
+                  alt=""
+                />
+              </div>
+              <span className="text-center text-xs ">No models created yet.</span>
             </div>
           </div>
         </div>
