6a6f14cde9df7a6a5f977ece8facdf7739e61dea
Chukwunonso Ikeji <williamikeji@gmail.com>
2023-11-03 10:08:20 +0100

    migrate: web3auth to particle.network (#106)
    * fixes/meta/seo
    
    * repository page ui fixed
    
    * initial commit
    
    * feat: add header to notification
    
    * feat: add notification avatars
    
    * feat: add notification data
    
    * feat: update Notification
    
    * feat: update notification page
    
    add notification component
    
    * fix : bug fixed
    
    * style: pretty code
    
    by ci
    
    * settings Ui Done
    
    * settings Ui Done
    
    * fix/dashboard
    
    * style: pretty code
    
    by ci
    
    * nvmrc:
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 7 minutes
    
    * notification ui error fixed
    
    * dashboard ui fixed
    
    * style: pretty code
    
    by ci
    
    * fixed zoom error
    
    * style: pretty code
    
    by ci
    
    * error fixed
    
    * style: pretty code
    
    by ci
    
    * peer dep issue
    
    * style: pretty code
    
    by ci
    
    ---------
    
    Signed-off-by: Nasfame <laciferin@gmail.com>
    Co-authored-by: Raji Roqeeb Olalekan <rajcodes733@gmail.com>
    Co-authored-by: sheriff Oladimeji <dimejiademola5@gmail.com>
    Co-authored-by: Sheriff-Oladimeji <Sheriff-Oladimeji@users.noreply.github.com>
    Co-authored-by: Codebox124 <Codebox124@users.noreply.github.com>
    Co-authored-by: Nasfame <laciferin@gmail.com>
    Co-authored-by: techyNonso <techyNonso@users.noreply.github.com>
    Co-authored-by: Nasfame <Nasfame@users.noreply.github.com>

---
+++ src/lib/particle.ts
@@ -0,0 +1,43 @@
+import { ParticleNetwork, WalletEntryPosition } from '@particle-network/auth'
+import { ParticleProvider } from '@particle-network/provider'
+// import { SolanaWallet } from '@particle-network/solana-wallet'
+import { Ethereum } from '@particle-network/chains'
+
+const particle = new ParticleNetwork({
+  projectId: `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+  clientKey: `${process.env.NEXT_PUBLIC_CLIENT_KEY}`,
+  appId: `${process.env.NEXT_PUBLIC_APP_ID}`,
+  chainName: Ethereum.name, //optional: current chain name, default Ethereum.
+  chainId: Ethereum.id, //optional: current chain id, default 1.
+  wallet: {
+    //optional: by default, the wallet entry is displayed in the bottom right corner of the webpage.
+    displayWalletEntry: true, //show wallet entry when connect particle.
+    defaultWalletEntryPosition: WalletEntryPosition.BR, //wallet entry position
+    uiMode: 'dark', //optional: light or dark, if not set, the default is the same as web auth.
+    supportChains: [
+      { id: 1, name: 'Ethereum' },
+      { id: 5, name: 'Ethereum' },
+    ], // optional: web wallet support chains.
+    customStyle: {}, //optional: custom wallet style
+  },
+  securityAccount: {
+    //optional: particle security account config
+    //prompt set payment password. 0: None, 1: Once(default), 2: Always
+    promptSettingWhenSign: 1,
+    //prompt set master password. 0: None(default), 1: Once, 2: Always
+    promptMasterPasswordSettingWhenLogin: 1,
+  },
+})
+
+const particleProvider = new ParticleProvider(particle.auth)
+//if you need support solana chain
+//const solanaWallet = new SolanaWallet(particle.auth)
+
+//if you use web3.js
+// window.web3 = new Web3(particleProvider)
+// window.web3.currentProvider.isParticleNetwork // => true
+
+//if you use ethers.js
+import { ethers } from 'ethers'
+const ethersProvider = new ethers.JsonRpcProvider(`${particleProvider}`) // new ethers.Jproviders.Web3Provider(particleProvider, 'any')
+const ethersSigner = ethersProvider.getSigner()
