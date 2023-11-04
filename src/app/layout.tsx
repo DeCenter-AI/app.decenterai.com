74624f07350d8e4c5b64df21a1636894b4f4c6b4
techyNonso <techyNonso@users.noreply.github.com>
2023-11-04 22:06:17 +0000

    style: pretty code
    by ci

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
-"use client"
+'use client'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { AppMetaData } from '@config/app'
-import { ModalProvider } from '@particle-network/connect-react-ui';
+import { ModalProvider } from '@particle-network/connect-react-ui'
 
 import { ContextProvider } from './userContext'
-import { WalletEntryPosition } from '@particle-network/auth';
-import { Ethereum, EthereumGoerli, EthereumSepolia, Polygon, PolygonMumbai } from '@particle-network/chains';
-import { evmWallets } from '@particle-network/connect';
+import { WalletEntryPosition } from '@particle-network/auth'
+import {
+  Ethereum,
+  EthereumGoerli,
+  EthereumSepolia,
+  Polygon,
+  PolygonMumbai,
+} from '@particle-network/chains'
+import { evmWallets } from '@particle-network/connect'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,32 +23,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-
         <ModalProvider
           options={{
             projectId: `${process.env.NEXT_PUBLIC_PARTICLE_NETWORK_PROJECT_ID}`,
             clientKey: `${process.env.NEXT_PUBLIC_PARTICLE_NETWORK_CLIENT_KEY}`,
             appId: `${process.env.NEXT_PUBLIC_PARTICLE_NETWORK_APP_ID}`,
-            chains: [
-              EthereumSepolia,
-              Polygon,
-              PolygonMumbai
-            ],
-            particleWalletEntry: {    //optional: particle wallet config
+            chains: [EthereumSepolia, Polygon, PolygonMumbai],
+            particleWalletEntry: {
+              //optional: particle wallet config
               displayWalletEntry: true, //display wallet button when connect particle success.
               defaultWalletEntryPosition: WalletEntryPosition.BR,
-              supportChains: [
-                EthereumSepolia,
-                Polygon,
-                PolygonMumbai
-              ],
+              supportChains: [EthereumSepolia, Polygon, PolygonMumbai],
               customStyle: {}, //optional: custom wallet style
             },
-            securityAccount: { //optional: particle security account config
+            securityAccount: {
+              //optional: particle security account config
               //prompt set payment password. 0: None, 1: Once(default), 2: Always
               promptSettingWhenSign: 1,
               //prompt set master password. 0: None(default), 1: Once, 2: Always
-              promptMasterPasswordSettingWhenLogin: 1
+              promptMasterPasswordSettingWhenLogin: 1,
             },
             // wallets: evmWallets({
             //   projectId: 'walletconnect projectId', //replace with walletconnect projectId
@@ -50,18 +49,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             // }),
           }}
           theme={'auto'}
-          language={'en'}   //optional：localize, default en
+          language={'en'} //optional：localize, default en
           walletSort={['Particle Auth', 'Wallet']} //optional：walelt order
-          particleAuthSort={[    //optional：display particle auth items and order
+          particleAuthSort={[
+            //optional：display particle auth items and order
             'email',
             'google',
-
           ]}
         >
-          <ContextProvider>
-            {children}
-          </ContextProvider>
-
+          <ContextProvider>{children}</ContextProvider>
         </ModalProvider>
       </body>
     </html>
