91c3415d6ecfcfd9ae5d9327e030740097ba3d7a
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-04 07:34:31 +0100

    feat: update profile settings

--- src/app/dashboard/settings/profile/page.tsx
+++ src/app/dashboard/settings/profile/page.tsx
@@ -2,20 +2,63 @@
 
 import { useRouter } from 'next/navigation'
 import { DashLayout } from '../../dashLayout'
-import {PiCaretLeft} from "react-icons/pi"
+import { PiCaretLeft, PiPencilSimpleLight } from 'react-icons/pi'
+import Image from 'next/image'
+import avatars from '@public/avatars'
+import { useState } from 'react'
 export default function Page() {
   const router = useRouter()
+  const [isModalOpen, setIsModalOPen] = useState(false)
+  const [showBackdrop, setShowBackdrop] = useState(false)
+  const openModal = () => {
+    setIsModalOPen(true)
+      setShowBackdrop(true)
+  }
 
+  const closeModal = () => {
+    setIsModalOPen(false)
+     setShowBackdrop(false)
+  }
   return (
     <DashLayout>
-      <section>
-        <div className='flex gap-4  items-center'>
+      {isModalOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 z-40 backdrop-blur-sm "
+          onClick={closeModal}
+        />
+      )}
+      <section className="h-full w-full py-4 flex flex-col gap-4">
+        <div className="flex gap-4  items-center text-[#8F8F8F] text-sm">
           <button
             type="button"
-            onClick={() => router.push('/dashboard/settings/profile')} className='flex gap-2 items-center'>
-            <PiCaretLeft/>
+            onClick={() => router.push('/dashboard/settings/profile')}
+            className="flex gap-2 items-center border-none outline-none">
+            <PiCaretLeft />
             <span>Back</span>
           </button>
+          <p>
+            Settings / <span className="text-primary_5">Profile</span>
+          </p>
+        </div>
+        <div className="flex justify-between items-center  rounded-2xl px-4 py-2 border border-primary_8 ">
+          <h2 className="font-logirentBold text-2xl uppercase">Profile</h2>
+          <button className=" bg-primary_10 rounded-full px-4 py-2 ">
+            Delete Account
+          </button>
+        </div>
+        <div className="w-[80%] sm:w-[70%] md:w-[60%]  border border-primary_8 rounded-xl py-2">
+          <div className="flex items-center gap-2">
+            <Image src={avatars.avatar} alt="" />
+            <div>
+              <h3 className="text-primary_5">David Ayegoro</h3>
+              <p className="text-primary_7">Dayegoro@gmail.com</p>
+            </div>
+            <button
+              className="flex items-center justify-center p-2 bg-primary_7 text-primary_6 rounded-full"
+              onClick={openModal}>
+              <PiPencilSimpleLight size={25} />
+            </button>
+          </div>
         </div>
       </section>
     </DashLayout>
--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,10 +1,13 @@
+import React from 'react'
+
 interface Info {
   id: number
   text: string
-  count: number
-  icon: React.ReactNode
+  count: string
 }
+
 interface Model {
+  id: number
   title: string
   name: string
   desc: string
@@ -12,3 +15,60 @@ interface Model {
   download: number
   like: number
 }
+
+const profileInfo: Info[] = [
+  {
+    id: 1,
+    text: 'Earnings',
+    count: '$12,000',
+  },
+  {
+    id: 2,
+    text: 'Teams',
+    count: '2',
+  },
+  {
+    id: 3,
+    text: 'Models',
+    count: '4',
+  },
+]
+
+const models: Model[] = [
+  {
+    id: 1,
+    title: 'Text Generation',
+    name: 'Model name',
+    desc: 'Identifies a variety of concepts in images and video including objects, themes, and more. ',
+    date: 'Updated Sep, 12 2023',
+    download: 12,
+    like: 127
+  },
+  {
+    id: 2,
+    title: 'Text Generation',
+    name: 'Model name',
+    desc: 'Identifies a variety of concepts in images and video including objects, themes, and more. ',
+    date: 'Updated Sep, 12 2023',
+    download: 12,
+    like: 127
+  },
+  {
+    id: 3,
+    title: 'Text Generation',
+    name: 'Model name',
+    desc: 'Identifies a variety of concepts in images and video including objects, themes, and more. ',
+    date: 'Updated Sep, 12 2023',
+    download: 12,
+    like: 127
+  },
+  {
+    id: 4,
+    title: 'Text Generation',
+    name: 'Model name',
+    desc: 'Identifies a variety of concepts in images and video including objects, themes, and more. ',
+    date: 'Updated Sep, 12 2023',
+    download: 12,
+    like: 127
+  },
+]
