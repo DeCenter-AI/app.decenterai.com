e2f8e4198aeac47731f4e3c6e5fd50258a6485da
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-04 16:42:37 +0100

    fix : change width for all elements

--- src/app/dashboard/settings/profile/page.tsx
+++ src/app/dashboard/settings/profile/page.tsx
@@ -3,11 +3,13 @@
 import { useRouter } from 'next/navigation'
 import { DashLayout } from '../../dashLayout'
 import { PiCaretLeft, PiCpu, PiPencilSimpleLight, PiUsersThree } from 'react-icons/pi'
+import {AiOutlineDownload, AiOutlineHeart} from "react-icons/ai"
 import { BiMoney } from 'react-icons/bi'
 import Image from 'next/image'
 import avatars from '@public/avatars'
 import { useState } from 'react'
-import { profileInfo } from '@/data/profile'
+import { models, profileInfo } from '@/data/profile'
+import box from "public/model-box.png"
 export default function Page() {
   const router = useRouter()
   const [isModalOpen, setIsModalOPen] = useState<boolean>(false)
@@ -48,42 +50,69 @@ export default function Page() {
             Delete Account
           </button>
         </div>
-        <div className="w-[80%] sm:w-[70%] md:w-[60%]  border border-primary_8 rounded-xl py-3 px-4">
-          <div className="flex items-center gap-2">
-            <Image src={avatars.avatar} alt=""  className='w-[20%]'/>
-            <div>
-              <h3 className="text-primary_5">David Ayegoro</h3>
-              <p className="text-primary_7">Dayegoro@gmail.com</p>
+        <main className='sm:w-[80%]'>
+          <div className=" border border-primary_8 rounded-xl py-3 px-4 flex gap-8">
+            <div className="flex items-center gap-2">
+              <Image src={avatars.avatar} alt="" />
+              <div>
+                <h3 className="text-primary_5">David Ayegoro</h3>
+                <p className="text-primary_7">Dayegoro@gmail.com</p>
+              </div>
             </div>
             <button
               className="flex items-center justify-center p-2 bg-primary_7 text-primary_6 rounded-full"
               onClick={openModal}>
-              <PiPencilSimpleLight size={25} />
+              <PiPencilSimpleLight size={20} />
             </button>
           </div>
-        </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 w-[80%] sm:w-[70%] md:w-[60%] gap-8">
-          {profileInfo.map((item) => (
-            <div
-              key={item.id}
-              className="border border-primary_8 rounded-xl py-2 w-full flex flex-col gap-6 px-4">
-              <p>{item.text}</p>
-              <div className="flex justify-between items-center">
-                <h4 className="text-lg">{item.count}</h4>
-                <div>
-                  {item.id === 1 ? (
-                    <BiMoney size={25} />
-                  ) : item.id === 2 ? (
-                    <PiUsersThree size={25} />
-                  ) : (
-                    <PiCpu size={25} />
-                  )}
+          <div className="grid sm:grid-cols-2 md:grid-cols-3  gap-8">
+            {profileInfo.map((item) => (
+              <div
+                key={item.id}
+                className="border border-primary_8 rounded-xl py-2 w-full flex flex-col gap-6 px-4">
+                <p className="text-[#C1C1C1]">{item.text}</p>
+                <div className="flex justify-between items-center">
+                  <h4 className="text-lg">{item.count}</h4>
+                  <div className="text-[#5D5D5D]">
+                    {item.id === 1 ? (
+                      <BiMoney size={30} />
+                    ) : item.id === 2 ? (
+                      <PiUsersThree size={30} />
+                    ) : (
+                      <PiCpu size={30} />
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+          <div className="overflow-y-auto  grid sm:grid-cols-2 md:grid-cols-2  gap-8 pr-2">
+            {models.map((model) => (
+              <div
+                key={model.id}
+                className="w-full p-2 flex gap-4 items-center border border-primary_8 rounded-xl">
+                <div className="flex flex-col gap-3">
+                  <span>{model.title}</span>
+                  <h4>{model.name}</h4>
+                  <small>{model.desc}</small>
+                  <div className="text-[#5D5D5D] flex gap-4 items-center text-[12px]">
+                    <span>{model.date}</span>
+                    <span className="flex gap-2 items-center">
+                      <AiOutlineDownload />
+                      {model.download}
+                    </span>
+                    <span className="flex gap-2 items-center">
+                      <AiOutlineHeart />
+                      {model.like}
+                    </span>
+                  </div>
+                </div>
+                <Image src={box} alt="model image" />
+              </div>
+            ))}
+          </div>
+        </main>
       </section>
     </DashLayout>
   )
