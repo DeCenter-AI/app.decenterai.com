2330fdf3f06927379ea17c334bbffc8ba844edee
techyNonso <techyNonso@users.noreply.github.com>
2023-11-06 07:19:58 +0000

    style: pretty code
    by ci

--- src/app/dashboard/components/emptyRepo.tsx
+++ src/app/dashboard/components/emptyRepo.tsx
@@ -3,10 +3,9 @@ import Image from 'next/image'
 import Emptyimg from '@public/magnify.png'
 export default function EmptyRepository() {
   return (
-    <div className='flex flex-col justify-center items-center  h-full'>
-
-      <div className='mb-2'>
-        <Image src={Emptyimg} className='grayscale' height={136} width={171.67} alt='' />
+    <div className="flex flex-col justify-center items-center  h-full">
+      <div className="mb-2">
+        <Image src={Emptyimg} className="grayscale" height={136} width={171.67} alt="" />
       </div>
       <span className="text-center text-xs ">No models created yet.</span>
     </div>
