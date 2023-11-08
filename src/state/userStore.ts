e7f6a92637ff5bd51d895eab6404e207799b0c96
Nasfame <Nasfame@users.noreply.github.com>
2023-11-08 21:01:59 +0000

    style: pretty code
    by ci

--- src/state/userStore.ts
+++ src/state/userStore.ts
@@ -1,7 +1,7 @@
-import {create} from 'zustand'
-import {devtools, persist} from 'zustand/middleware'
-import {upsert_user} from "@app/explore/upsert_user";
-import {User} from '@prisma/client';
+import { create } from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
+import { upsert_user } from '@app/explore/upsert_user'
+import { User } from '@prisma/client'
 
 // interface IUser{
 //     id: string
@@ -14,42 +14,42 @@ import {User} from '@prisma/client';
 //     userDB: User
 // }
 
-type IUser = Omit<Omit<User, 'createdAt'>,'updatedAt'>
+type IUser = Omit<Omit<User, 'createdAt'>, 'updatedAt'>
 
 type Store = {
-    user: IUser | null
-    init: (user: IUser) => void
-    setUser: (user: IUser) => void
+  user: IUser | null
+  init: (user: IUser) => void
+  setUser: (user: IUser) => void
 }
 
 const useUserStore = create<Store>()(
-    devtools(
-        persist(
-            (set) => ({
-                user: null,
+  devtools(
+    persist(
+      (set) => ({
+        user: null,
 
-                init(userData: IUser) {
-                    set((state) => ({
-                        ...state,
-                        user: userData,
-                    }))
-                    console.log("userStore: init")
-                },
-                // TODO: setUser implement in profile/EditProfile
-                async setUser(userData: IUser) {
-                    set((state) => ({
-                        ...state,
-                        user: userData,
-                    }))
-                    console.log("userStore: setUser")
-                    await upsert_user(userData)
-                },
-            }),
-            {
-                name: 'userData',
-            },
-        ),
-    )
+        init(userData: IUser) {
+          set((state) => ({
+            ...state,
+            user: userData,
+          }))
+          console.log('userStore: init')
+        },
+        // TODO: setUser implement in profile/EditProfile
+        async setUser(userData: IUser) {
+          set((state) => ({
+            ...state,
+            user: userData,
+          }))
+          console.log('userStore: setUser')
+          await upsert_user(userData)
+        },
+      }),
+      {
+        name: 'userData',
+      },
+    ),
+  ),
 )
 
 export default useUserStore
