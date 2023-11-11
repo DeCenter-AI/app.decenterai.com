1cec066e7f7be2195d4d80e6377c06a4964ede3f
utkarsh-1602 <utkarsh-1602@users.noreply.github.com>
2023-11-11 13:30:04 +0000

    style: pretty code
    by ci

--- cypress/e2e/explore.cy.js
+++ cypress/e2e/explore.cy.js
@@ -1,18 +1,17 @@
 // cypress/integration/explorePage.spec.js
 describe('Explore Page', () => {
-    it('should log in and navigate to dashboard', () => {
-        cy.visit('/explore');
+  it('should log in and navigate to dashboard', () => {
+    cy.visit('/explore')
 
-        // Check if the page contains the logo
-        cy.get('img[alt="logo image"]').should('exist');
+    // Check if the page contains the logo
+    cy.get('img[alt="logo image"]').should('exist')
 
-        // Mock the login process
-        cy.intercept('POST', '/api/auth/login', {
-            fixture: 'loginResponse.json',
-        }).as('login');
+    // Mock the login process
+    cy.intercept('POST', '/api/auth/login', {
+      fixture: 'loginResponse.json',
+    }).as('login')
 
-        // Click the "Start here" button
-        cy.get('.border-primary_11').click();
-
-    });
-});
+    // Click the "Start here" button
+    cy.get('.border-primary_11').click()
+  })
+})
