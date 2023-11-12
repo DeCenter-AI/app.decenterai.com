48246bb43e76b46dcc2142e535bcd316e584d4d1
Codebox124 <Codebox124@users.noreply.github.com>
2023-11-12 20:13:12 +0000

    style: pretty code
    by ci

--- src/state/notificationStore.ts
+++ src/state/notificationStore.ts
@@ -1,14 +1,13 @@
 import { create } from 'zustand'
 
-
 type Notification = {
-    id: number;
-    message: string;
-    // type: 'success' | 'error' | 'info';
-};
+  id: number
+  message: string
+  // type: 'success' | 'error' | 'info';
+}
 
 type NotificationStore = {
-    notifications: Notification[];
-    addNotification: (notification: Notification) => void;
-    removeNotification: (id: number) => void;
-};
+  notifications: Notification[]
+  addNotification: (notification: Notification) => void
+  removeNotification: (id: number) => void
+}
