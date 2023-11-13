2d45eb49b70859737b6c7e80d177da85e45cb5a4
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-13 11:53:49 +0100

    feat: add exception to taning request route

--- src/app/api/prisma/training_request/route.ts
+++ src/app/api/prisma/training_request/route.ts
@@ -1,9 +1,15 @@
 import prisma from '@lib/prisma'
 
 export async function POST(req: Request) {
-    const  trainingRequest  = await req.json()
-  const create_request = await prisma.trainingRequest.create({
-    data: trainingRequest,
-  })
-  return new Response(JSON.stringify({ status: 200 }))
+  try {
+    const trainingRequest = await req.json()
+    const create_request = await prisma.trainingRequest.create({
+      data: trainingRequest,
+    })
+    return new Response(JSON.stringify({ status: 200 }))
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ message: 'Error creating training request', error, status: 500 }),
+    )
+  }
 }
