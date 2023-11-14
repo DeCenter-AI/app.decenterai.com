c0dcf9f61f5d2bc0ec110142dd18789c6400dba7
Raji Roqeeb Olalekan <rajcodes733@gmail.com>
2023-11-14 12:29:29 +0100

    old

--- src/state/notificationStore.ts
+++ src/state/notificationStore.ts
@@ -1,13 +1,29 @@
-import { create } from 'zustand'
+// import notifications from '@/data/notification';
+// import { create } from 'zustand'
 
-type Notification = {
-  id: number
-  message: string
-  // type: 'success' | 'error' | 'info';
-}
+// type Notification = {
+//     id: number;
+//     name: string;
+//     msg: string;
+//     avatar: string;
+//     time: string;
+//     read: boolean;
+//   };
 
-type NotificationStore = {
-  notifications: Notification[]
-  addNotification: (notification: Notification) => void
-  removeNotification: (id: number) => void
-}
+//   type NotificationStore = {
+//     notifications: Notification[];
+//     markAsRead: (id: number) => void;
+//   };
+  
+//   const useNotificationStore = create<NotificationStore>((set) => ({
+//   notifications: [...notifications], // Assuming notifications is your initial data
+//   markAsRead: (id) => {
+//     set((state) => ({
+//       notifications: state.notifications.map((notification) =>
+//         notification.id === id ? { ...notification, read: true } : notification
+//       ),
+//     }));
+//   },
+// }));
+
+// export default useNotificationStore;
