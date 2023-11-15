f9f8125b0f25565f8bc7736b062a0cf4c62659b4
techyNonso <williamikeji@gmail.com>
2023-11-15 20:33:33 +0100

    repositories migrated to store with some problems

--- src/app/dashboard/repository/components/repository.tsx
+++ src/app/dashboard/repository/components/repository.tsx
@@ -1,39 +1,43 @@
+import useModelStore, { IModel } from '@/state/modelStore'
 import React from 'react'
 import { BiLike } from 'react-icons/bi'
 import { GoDownload } from 'react-icons/go'
 
 export default function Repository({ repository }) {
+  const { models } = useModelStore()
+
   return (
     <div>
       <div className="">
-        {repository.map((item) => (
+        {/* extract the values of the object */}
+        {Object.values(models).map(model => (
           <div
-            key={item.id}
+            key={model.id}
             className=" border border-primary_9 flex px-5 mb-5 py-3 items-center rounded-xl  h-[170px] min-h-[170px]"
           >
             <div className="w-[80%] h-full">
               <div className=" flex h-[25%] items-start ">
                 <span className="bg-primary_11 rounded-full text-primary_8 py-1 px-3">
-                  {item.label}
+                  {'Text Generation'}
                 </span>
               </div>
               <div className="h-[20%]">
-                <span className="text-lg ">{item.title}</span>
+                <span className="text-lg ">{model.name}</span>
               </div>
               <div className="flex items-center h-[40%]">
-                <p className="text-xs">{item.description} </p>
+                <p className="text-xs">{model.description} </p>
               </div>
               <div className=" h-[15%] text-xs text-primary_8 flex gap-4">
                 <div>
-                  <span>Updated {item.time}</span>
+                  <span>Updated {"12 2023"}</span>
                 </div>
                 <div className="flex">
                   <GoDownload />
-                  <span>{item.dowloaded}</span>
+                  <span>{'12k'}</span>
                 </div>
                 <div className="flex">
                   <BiLike />
-                  <span>{item.likes}</span>
+                  <span>{'50'}</span>
                 </div>
               </div>
             </div>
