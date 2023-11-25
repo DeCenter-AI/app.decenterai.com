95588ef7043ec001bb64d39127f165cb5c5eeaff
utkarsh-1602 <usking619@gmail.com>
2023-11-25 19:35:50 +0530

    lighthouse upload image, fix needed

--- src/app/explore/welcome/page.tsx
+++ src/app/explore/welcome/page.tsx
@@ -4,11 +4,18 @@ import useUserStore from '@/state/userStore'
 import { myImageLoader } from '@/lib/imageHelper'
 import { AiOutlineCamera } from 'react-icons/ai'
 import { useRouter } from 'next/navigation'
+import lighthouse from "@lighthouse-web3/sdk"
+import { useState } from 'react'
+
+async function uploadImage(selectedImage) {
+    return await lighthouse.upload(selectedImage, process.env.NEXT_PUBLIC_LH_API);
+}
 
 export default function Page() {
 
     const { push } = useRouter()
     const userStore = useUserStore()
+    const [selectedImage, setSelectedImage] = useState(null);
 
     const handleNameChange = (event) => {
         if (userStore.user) {
@@ -18,17 +25,42 @@ export default function Page() {
         }
 
     }
-
     const handleBioChange = (event) => {
         userStore.setUser({
             bio: `${event.target.value}`,
         })
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        push('/dashboard')
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedImage(file);
+    };
+
+    type Url = {
+        url: string
     }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (selectedImage) {
+            try {
+                const response = await uploadImage(selectedImage);
+
+                console.log(response)
+
+                // Redirect to the dashboard
+                push('/dashboard');
+            } catch (error) {
+                console.error('Image upload failed', error);
+            }
+        } else {
+            // If no image is selected, proceed without uploading
+            push('/dashboard');
+        }
+    };
+
+
 
     return (
         <div className="absolute inset-0 z-50 bg-black w-[80%] sm:w-[60%] md:w-[40%] lg:w-[30%] h-[90%] max-h-screen mx-auto transition-all duration-500 ease-in-out rounded-xl px-6 py-6 flex flex-col gap-4 shadow-xl my-auto" style={{ border: '1px solid white' }}>
@@ -38,23 +70,31 @@ export default function Page() {
                 Welcome
             </h1>
             <div className="relative w-[25%] mx-auto">
-                <Image
-                    src={userStore.user.profileImage}
-                    alt="profile pic"
-                    loader={myImageLoader}
-                    unoptimized
-                    width={70}
-                    height={70}
-                    className="max-w-[100%] max-h-[100%] rounded-full "
+                <input
+                    type="file"
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    id="imageInput"
                 />
+                <label htmlFor="imageInput">
+                    <Image
+                        src={userStore.user.profileImage}
+                        alt="profile pic"
+                        loader={myImageLoader}
+                        unoptimized
+                        width={70}
+                        height={70}
+                        className="max-w-[100%] max-h-[100%] rounded-full cursor-pointer"
+                    />
+                </label>
 
                 <button className="absolute -bottom-2 left-10 text-primary_7 bg-primary_10 p-1.5 rounded-full">
-                    <AiOutlineCamera size={20} />
+                    <AiOutlineCamera size={20} onClick={handleImageChange} />
                 </button>
             </div>
-            <form action="" className="text-primary_8 text-sm flex flex-col gap-4 relative">
-                <div className="flex gap-4">
-                    <div className="flex flex-col gap-1">
+            <form action="" className="text-primary_8 text-sm flex flex-col gap-4 relative ">
+                <div className="flex gap-4" >
+                    <div className="flex flex-col gap-1 sm:flex-row sm:items-center sm:justify-between">
                         {/*FIXME: keep firstName and last name ; .*/}
                         <label htmlFor="name" className="font-bold text-sm text-primary_2">
                             First Name
@@ -67,7 +107,7 @@ export default function Page() {
                         />
 
                     </div>
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 sm:flex-row sm:items-center sm:justify-between " >
                         {/*FIXME: keep firstName and last name ; .*/}
                         <label htmlFor="name" className="font-bold text-sm text-primary_2">
                             Last Name
