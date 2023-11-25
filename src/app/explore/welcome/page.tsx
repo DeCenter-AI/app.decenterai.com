bc65ed9975dfb1b010a4179288d2d475095a91d5
utkarsh-1602 <usking619@gmail.com>
2023-11-25 18:51:07 +0530

    /explore/welcome, tailwind csss config colors added

--- src/app/explore/welcome/page.tsx
+++ src/app/explore/welcome/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
 
     return (
         <div className="absolute inset-0 z-50 bg-black w-[80%] sm:w-[60%] md:w-[40%] lg:w-[30%] h-[90%] max-h-screen mx-auto transition-all duration-500 ease-in-out rounded-xl px-6 py-6 flex flex-col gap-4 shadow-xl my-auto" style={{ border: '1px solid white' }}>
-            <h1 className="font-logirentBold text-primary_1 text-center font-bold text-5xl text-black rounded-md py-3 w-[80%] lg:w-[100%] h-[10%]"
+            <h1 className="font-logirentBold text-primary_1 text-center font-bold text-5xl rounded-md py-3 w-[80%] lg:w-[100%] h-[10%]"
             >
                 {' '}
                 Welcome
@@ -48,11 +48,11 @@ export default function Page() {
                     className="max-w-[100%] max-h-[100%] rounded-full "
                 />
 
-                <button className="absolute -bottom-2 left-10 text-[#8F8F8F] bg-[#2B2B2B] p-1.5 rounded-full">
+                <button className="absolute -bottom-2 left-10 text-primary_7 bg-primary_10 p-1.5 rounded-full">
                     <AiOutlineCamera size={20} />
                 </button>
             </div>
-            <form action="" className="text-[#5D5D5D] text-sm flex flex-col gap-4 relative">
+            <form action="" className="text-primary_8 text-sm flex flex-col gap-4 relative">
                 <div className="flex gap-4">
                     <div className="flex flex-col gap-1">
                         {/*FIXME: keep firstName and last name ; .*/}
@@ -63,7 +63,7 @@ export default function Page() {
                             type="text"
                             value={userStore.user?.name}
                             onChange={handleNameChange}
-                            className="text-[#8F8F8F] border border-primary_8 rounded-xl p-2 focus:outline-none bg-transparent"
+                            className="text-primary_7 border border-primary_8 rounded-xl p-2 focus:outline-none bg-transparent"
                         />
 
                     </div>
@@ -76,7 +76,7 @@ export default function Page() {
                             type="text"
                             value={userStore.user?.name}
                             onChange={handleNameChange}
-                            className="text-[#8F8F8F] border border-primary_8 rounded-xl p-2 focus:outline-none bg-transparent"
+                            className="text-primary_7 border border-primary_8 rounded-xl p-2 focus:outline-none bg-transparent"
                         />
                     </div>
                 </div>
@@ -88,7 +88,7 @@ export default function Page() {
                         type="text"
                         placeholder="Nick name (optional)"
                         value={userStore.user ? userStore.user.name : ''}
-                        className="text-[#8F8F8F] border border-primary_8 rounded-xl p-2 focus:outline-none bg-transparent"
+                        className="text-primary_7 border border-primary_8 rounded-xl p-2 focus:outline-none bg-transparent"
                         id="Username"
                     />
                 </div>
@@ -99,7 +99,7 @@ export default function Page() {
                     <input
                         type="text"
                         value={userStore.user?.email}
-                        className="text-[#8F8F8F] border border-primary_8 rounded-xl p-2 focus:outline-none bg-transparent"
+                        className="text-primary_7 border border-primary_8 rounded-xl p-2 focus:outline-none bg-transparent"
                         id="email"
                         name="email"
                     />
@@ -112,7 +112,7 @@ export default function Page() {
                         name="bio"
                         id="bio"
                         value={userStore.user?.bio}
-                        className="text-[#8F8F8F] border border-primary_8 rounded-xl p-3 focus:outline-none bg-transparent h-12 overflow-y-hidden"
+                        className="text-primary_7 border border-primary_8 rounded-xl p-3 focus:outline-none bg-transparent h-12 overflow-y-hidden"
                         placeholder="About you (optional)"
                         onChange={handleBioChange}
                     ></textarea>
